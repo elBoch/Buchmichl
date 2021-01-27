232ca457406ad706767b91e5b939e4600453a3ba
Thomas Kainz <kaithd17@htl-kaindorf.at>
2021-01-27 18:38:39 +0100

    database access

--- Buchmichl_Projekt/server.js
+++ Buchmichl_Projekt/server.js
@@ -16,7 +16,15 @@ app.use(require('./routes/list'));
 app.use(require('./routes/filter'));
 app.use(require('./routes/ansicht'));
 
-
+var pgp = require("pg-promise")(/*/options/*/);
+var db = pgp("postgresql://postgres:postgres@127.0.0.1/buchmichl");
+db.one("SELECT * FROM benutzer WHERE userid=1")
+    .then(function (data) {
+        console.log("DATA:", data.username);
+    })
+    .catch(function (error) {
+        console.log("ERROR:", error);
+});
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
