6f939b31e5247c3394f4c74259afea0f6770f0b8
Matthias <hormad17@htl-kaindorf.at>
2021-02-04 14:21:56 +0100

    Zusammenführung abgeschlossen

--- Buchmichl_Projekt/public/view/filterclient.js
+++ Buchmichl_Projekt/public/view/filterclient.js
@@ -27,3 +27,33 @@ $(document).ready(function(){
 
 
 });
+
+$(document).ready(function(){
+    $("#home").on('click',function(e){
+        console.log("here we go");
+
+        e.preventDefault();
+        //var data = $('input[name=quote]').val();
+        $.ajax({
+            type: 'GET',
+            url: '/',
+            //contentType: 'application/json',
+            //data: "test",
+            success: function(data) {
+                console.log("Successfully saved the matched beans to the user.");
+                window.location='/';
+            }
+        }).done(function ( ) {
+            console.log("OK");
+            
+        }).fail(function ( jqXHR, textStatus, errorThrown ) {
+            console.log(jqXHR);
+            console.log(textStatus);
+            console.log(errorThrown);
+        });
+
+
+    });
+
+
+});
--- Buchmichl_Projekt/public/view/startclient.js
+++ Buchmichl_Projekt/public/view/startclient.js
@@ -25,5 +25,33 @@ $(document).ready(function(){
 
     });
 
+});
+
+$(document).ready(function(){
+    $("#filter").on('click',function(e){
+        console.log("here we go");
+
+        e.preventDefault();
+        //var data = $('input[name=quote]').val();
+        $.ajax({
+            type: 'GET',
+            url: '/filter',
+            //contentType: 'application/json',
+            //data: "test",
+            success: function(data) {
+                console.log("Successfully saved the matched beans to the user.");
+                window.location='/filter';
+            }
+        }).done(function ( ) {
+            console.log("OK");
+            
+        }).fail(function ( jqXHR, textStatus, errorThrown ) {
+            console.log(jqXHR);
+            console.log(textStatus);
+            console.log(errorThrown);
+        });
+
+
+    });
 
 });
