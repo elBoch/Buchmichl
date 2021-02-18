8f4537a9519562f12d81b27f2ac9f21687f80b8e
Matthias <hormad17@htl-kaindorf.at>
2021-02-18 14:36:12 +0100

    login wirklich verbessert

--- Buchmichl_Projekt/routes/filter.js
+++ Buchmichl_Projekt/routes/filter.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var pgp = require("pg-promise")(/*/options/*/);
-var db = pgp("postgresql://postgres:postgres@127.0.0.1/buchmichl");
 
 
 router.get('/filter', (req, res) => {
@@ -11,23 +9,5 @@ router.get('/filter', (req, res) => {
     });
 });
 
-
-router.post('/authenticate',(req,res)=>{
-  db.one("SELECT passwort FROM benutzer WHERE username=$1",[req.body.name])
-      .then(function (data) {
-
-        if(data.passwort == req.body.passw){
-            res.send({query:'ok'});
-        }
-        else{
-            res.send({query:'nicht ok'});
-        }
-      })
-      .catch(function (error) {
-        console.log("ERROR:", error);
-        res.send({query:'nicht ok'});
-      });
-
-});
 
 module.exports = router;
--- Buchmichl_Projekt/routes/login.js
+++ Buchmichl_Projekt/routes/login.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var pgp = require("pg-promise")(/*/options/*/);
+var db = pgp("postgresql://postgres:postgres@127.0.0.1/buchmichl");
 
 
 router.get('/login', (req, res) => {
@@ -8,4 +10,22 @@ router.get('/login', (req, res) => {
         pageTitle: 'Login',
     });
 });
+
+router.post('/authenticate',(req,res)=>{
+    db.one("SELECT passwort FROM benutzer WHERE username=$1",[req.body.name])
+        .then(function (data) {
+  
+          if(data.passwort == req.body.passw){
+              res.send({query:'ok'});
+          }
+          else{
+              res.send({query:'nicht ok'});
+          }
+        })
+        .catch(function (error) {
+          //console.log("ERROR:", error);
+          res.send({query:'nicht ok'});
+        });
+  
+  });
 module.exports = router;
