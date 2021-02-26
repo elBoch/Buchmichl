32102d59eb65eada3784e26a1deeb29c03061830
Matthias <hormad17@htl-kaindorf.at>
2021-02-26 16:27:46 +0100

    rigistrierung und filter optimiert

--- Buchmichl_Projekt/routes/filter.js
+++ Buchmichl_Projekt/routes/filter.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var db1 = require('./login').db;
+var filterStatement="";
 
 
 router.get('/filter', (req, res) => {
@@ -13,12 +14,14 @@ router.get('/filter', (req, res) => {
 
 router.post('/getUnterkunftList', (req, res) => {
     //console.log(req.body);
-      db1.query('SELECT * FROM unterkunft u INNER JOIN zimmerartinunterkunft zu ON u.unterkunftid=zu.unterkunftid INNER JOIN zimmer z ON z.zimmerartid = zu.zimmerartid AND z.unterkunftid= zu.unterkunftid INNER JOIN unterkunftart ua ON u.unterkunftartid = ua.unterkunftartid WHERE u.sterne=$1 AND u.regionid=(SELECT regionid FROM region WHERE regionname=$2) AND z.preis BETWEEN $3 AND $4 AND z.anzahlpersonen=$5 AND ua.unterkunftartname = $6;',
-      [req.body.sterne,req.body.region, req.body.preis_von, req.body.preis_bis, req.body.anz_pers, req.body.unterkunftart])
+    buildStatement(req);
+    //console.log(filterStatement);
+      db1.query(filterStatement)
         .then( (data) => {
   
           //console.log(data);
          res.send(data);
+         filterStatement="";
         })
         .catch( (error) => {
           console.log("ERROR:", error);
@@ -26,6 +29,38 @@ router.post('/getUnterkunftList', (req, res) => {
         });
   
   });
+
+
+  
+  function buildStatement(req){
+    filterStatement +=" SELECT * "+
+                      "FROM unterkunft u INNER JOIN zimmerartinunterkunft zu ON u.unterkunftid=zu.unterkunftid "+
+                                        "INNER JOIN zimmer z ON z.zimmerartid = zu.zimmerartid AND z.unterkunftid= zu.unterkunftid "+ 
+                                        "INNER JOIN unterkunftart ua ON u.unterkunftartid = ua.unterkunftartid "+
+                      "WHERE z.preis BETWEEN "+req.body.preis_von+" AND ";
+
+    if(req.body.preis_bis !=""){
+      filterStatement+=req.body.preis_bis;
+    }
+    else{
+      filterStatement+="1000000";
+    }
+    if(req.body.sterne!="Egal"){
+      filterStatement+=" AND u.sterne="+req.body.sterne;
+    }
+    if(req.body.region!=""){
+      filterStatement+=" AND u.regionid=(SELECT regionid FROM region WHERE regionname='"+req.body.region+"')";
+    }
+    if(req.body.anz_pers!=""){
+      filterStatement+=" AND z.anzahlpersonen="+req.body.anz_pers;
+    }
+    if(req.body.unterkunftart!="Egal"){
+      filterStatement+=" AND ua.unterkunftartname = '"+req.body.unterkunftart+"'";
+    }
+
+    filterStatement+";";
+
+  }
 
 
 module.exports = router;
--- Buchmichl_Projekt/routes/login.js
+++ Buchmichl_Projekt/routes/login.js
@@ -6,43 +6,58 @@ var passwordHash = require('password-hash');
 
 router.get('/login', (req, res) => {
 
-    res.render('login.ejs', {
-        pageTitle: 'Login',
-    });
+  res.render('login.ejs', {
+    pageTitle: 'Login',
+  });
 });
 
-router.post('/authenticate',(req,res) => {
+router.post('/authenticate', (req, res) => {
   console.log(req.body.name);
-    db.query("SELECT passwort FROM benutzer WHERE username=$1",[req.body.name])
-        .then( (data) => {
-          console.log(data[0].passwort);
-  
-          if(passwordHash.verify(req.body.passw, data[0].passwort)){
-              res.send({query:'ok'});
-          }
-          else{
-              res.send({query:'nicht ok'});
-          }
-        })
-        .catch( (error) => {
-          //console.log("ERROR:", error);
-          res.send({query:'nicht ok'});
-        });
-  
-  });
+  db.query("SELECT passwort FROM benutzer WHERE username=$1", [req.body.name])
+    .then((data) => {
+      //console.log(data[0].passwort);
+
+      if (passwordHash.verify(req.body.passw, data[0].passwort)) {
+        res.send({ query: 'ok' });
+      }
+      else {
+        res.send({ query: 'nicht ok' });
+      }
+    })
+    .catch((error) => {
+      //console.log("ERROR:", error);
+      res.send({ query: 'nicht ok' });
+    });
+
+});
 
 router.post('/register', (req, res) => {
   var hashedPassword = passwordHash.generate(req.body.passw);
-    db.query('INSERT INTO benutzer (username, passwort, vorname, nachname, geburtsdatum, addresse, email, admin) VALUES($1,$2, \'vorname\',\'nachname\',\'01.01.2020\',\'addresse\',$3,\'false\')',
-    [req.body.name,hashedPassword , req.body.email])
-    .then((data)=>{
-      console.log("sdjfg");
-      res.send('register successed');
+  db.query('SELECT COUNT(*) FROM benutzer WHERE username = $1', [req.body.name])
+    .then((data) => {
+      //console.log(data[0].count);
+      if (data[0].count !=0) {
+        res.send("username bereits vergeben");
+      }
+      else {
+        db.query('INSERT INTO benutzer (username, passwort, vorname, nachname, geburtsdatum, addresse, email, admin) VALUES($1,$2, \'vorname\',\'nachname\',\'01.01.2020\',\'addresse\',$3,\'false\')',
+          [req.body.name, hashedPassword, req.body.email])
+          .then((data) => {
+            //console.log("sdjfg");
+            res.send('register successed');
+          })
+          .catch((error) => {
+            console.log("ERROR:", error);
+            res.send('register failed');
+          });
+      }
     })
-    .catch( (error) => {
+    .catch((error) => {
       console.log("ERROR:", error);
       res.send('register failed');
     });
+
+
 });
 module.exports = router;
 module.exports.db = db;
