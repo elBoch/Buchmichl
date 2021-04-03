a461329091a48a112f5de930a8094c8d4046586e
Thomas Kainz <kaithd17@htl-kaindorf.at>
2021-04-03 15:31:08 +0200

    progress detailedRoom

--- public/view/detailedRoomclient.js
+++ public/view/detailedRoomclient.js
@@ -3,17 +3,14 @@ $(document).ready(function(){
         console.log("here we go");
 
         e.preventDefault();
-        //var data = $('input[name=quote]').val();
         $.ajax({
             type: 'GET',
             url: '/',
-            //contentType: 'application/json',
-            //data: "test",
             success: function(data) {
                 console.log("Successfully saved the matched beans to the user.");
                 window.location='/';
             }
-        }).done(function ( ) {
+        }).done(function() {
             console.log("OK");
             
         }).fail(function ( jqXHR, textStatus, errorThrown ) {
@@ -33,9 +30,9 @@ function currentDiv(n) {
   }
   
   function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
+    let i;
+    let x = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("demo");
     if (n > x.length) {slideIndex = 1}
     if (n < 1) {slideIndex = x.length}
     for (i = 0; i < x.length; i++) {
--- routes/detailedRoom.js
+++ routes/detailedRoom.js
@@ -1,52 +1,53 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const client = require("./login").client;
 let unterkunft;
 
-router.get('/detailedRoom', async (req, res) => {
-    const unterkunftView = await client.query(
-        "SELECT * FROM unterkunft WHERE unterkunftname=$1",
-        [unterkunft]
-    );
-    const einrichtungen = await client.query(
-        "SELECT einrichtungsname FROM einrichtung e INNER JOIN UnterkunftsEinrichtung ue ON e.einrichtungsid = ue.einrichtungsid INNER JOIN unterkunft u ON ue.unterkunftid = u.unterkunftid WHERE unterkunftname = $1",
-        [unterkunft]
-    );
+router.get("/detailedRoom", async (req, res) => {
+  const unterkunftView = await client.query(
+    "SELECT * FROM unterkunft WHERE unterkunftname=$1",
+    [unterkunft]
+  );
+  const unterkunftData = await client.query(
+    "SELECT e.einrichtungsname, ua.unterkunftartname, r.regionname, fa.freizeitausstattungsname, u.sterne, s.sehenswuerdigkeitsname FROM einrichtung e INNER JOIN unterkunftseinrichtung ue ON e.einrichtungsid = ue.einrichtungsid INNER JOIN unterkunft u ON ue.unterkunftid = u.unterkunftid INNER JOIN unterkunftart ua ON u.unterkunftid = ua.unterkunftartid INNER JOIN region r ON r.regionid = u.regionid INNER JOIN freizeitausstattungregion far ON far.regionid = r.regionid INNER JOIN freizeitausstattung fa ON fa.freizeitausstattungid = far.freizeitausstattungid INNER JOIN sehenswuerdigkeiten s ON s.regionid = r.regionid WHERE unterkunftname = $1;",
+    [unterkunft]
+  );
+  let einrichtungsString = buildString(unterkunftData.rows, "einrichtungsname");
+  let sterneString = buildString(unterkunftData.rows, "sterne");
+  let unterkunftartString = buildString(unterkunftData.rows, "unterkunftartname");
+  let regionString = buildString(unterkunftData.rows, "regionname");
+  let freizeitausstattungString = buildString(unterkunftData.rows,"freizeitausstattungsname");
+  let sehenswuerdigkeitString = buildString(unterkunftData.rows,"sehenswuerdigkeitsname");
 
-    //console.log(einrichtungen.rows);
-    let einrichtungsString = buildString(einrichtungen.rows,"einrichtungsname");
-
-
-    res.render("detailedRoom.ejs", {
-        pageTitle: "Detail",
-        unterkunftname: unterkunftView.rows[0].unterkunftname,
-        unterkunftstext: unterkunftView.rows[0].unterkunftstext,
-        einrichtungen : einrichtungsString,
-    });
+  res.render("detailedRoom.ejs", {
+    pageTitle: "Detail",
+    unterkunftname: unterkunftView.rows[0].unterkunftname,
+    unterkunftstext: unterkunftView.rows[0].unterkunftstext,
+    einrichtungen: einrichtungsString,
+    sterne: sterneString,
+    unterkunftart: unterkunftartString,
+    region: regionString,
+    freizeitausstattung: freizeitausstattungString,
+    sehenswuerdigkeit: sehenswuerdigkeitString
+  });
 });
 
 router.post("/detailedRoom", (req, res) => {
-    unterkunft = req.body.unterkunft;
-
+  unterkunft = req.body.unterkunft;
 });
 
-
-
-
-function buildString(data,type) {
-    //console.log(data)
-    let dataString="";
-    for (let i = 0; i < data.length; i++) {
-
-        if (i == data.length - 1) {
-            dataString += data[i][type];
-        }
-        else {
-            dataString += data[i][type] + ",";
-        }
+const buildString = (data, type) => {
+  let dataString = "";
+  for (let i = 0; i < data.length; i++) {
+    if (i == data.length - 1) {
+      dataString += data[i][type];
+    } else {
+      if (data[i][type] != data[i + 1][type]) {
+        dataString += data[i][type] + ", ";
+      }
     }
-    console.log(dataString);
-    return dataString;
-}
+  }
+  return dataString;
+};
 
 module.exports = router;
