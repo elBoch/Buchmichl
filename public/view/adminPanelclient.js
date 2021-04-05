b347ea2e73eeabf9dec784b58d6e46f663e0b6ba
= <trumad17@htl-kaindorf.at>
2021-04-05 11:18:40 +0200

    adminPanel refined

--- public/view/adminPanelclient.js
+++ public/view/adminPanelclient.js
@@ -21,3 +21,21 @@ $(document).ready(() => {
         });
     });
   });
+
+  window.onload=() => {
+    $.ajax({
+      type: "GET",
+      url: "/checkIfAuthenticated",
+      success: function (data) {
+        console.log("Successfully saved the matched beans to the user.Authenticted");
+      },
+    })
+      .done(() => {
+        console.log("OK");
+      })
+      .fail((jqXHR, textStatus, errorThrown) => {
+        console.log(jqXHR);
+        console.log(textStatus);
+        console.log(errorThrown);
+      });
+};
